38502b25ded772c34e26bb3eb8f3e0e83134bcf7
ash6218 <ash6218@gmail.com>
2025-08-11 22:49:35 +0330

    before 3rd deploy

--- utils.py
+++ utils.py
@@ -4,9 +4,6 @@ from django.contrib.auth.mixins import UserPassesTestMixin
 def send_otp_code(phone_number, code, request):
     try:
         api = KavenegarAPI(request.session['sms_p']['0'])
-        #'09201140293': 416964687674356D3756323165612F5170337152684C5636625A6F75452F4C6167364B4336437A4B6F67773D
-        #'09982440293': 6D594336547453695931705838726556703369506F6F3269542F363735382B79677650424F355A6C3352773D
-        #'09355480293': 55514C3077426359392B2B6266452B5A316749575059544C2B504C2F304E4C394276493231555736466A6B3D
         params = { 'sender' : '2000660110', 'receptor': phone_number, 'message' :f'کد تایید شما: {code}' }
         response = api.sms_send(params)
 
